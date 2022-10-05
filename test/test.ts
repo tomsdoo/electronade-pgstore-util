367afd26269c251790ebe299c21102bb831a3567
tom <toms@live.jp>
2022-10-05 13:50:55 +0900

    format:test

--- test/test.ts
+++ test/test.ts
@@ -6,23 +6,27 @@ let store: PgStore;
 
 describe("PgStore", () => {
   before(() => {
-    const [
-      connectionString,
-      tableName
-    ] = [
-      "posgtres://...",
-      "tableName"
-    ];
+    const [connectionString, tableName] = ["posgtres://...", "tableName"];
     store = new PgStore(connectionString, tableName);
 
     // @ts-ignore
     globalThis.electronade = {
       pgstore: {
-        get: async (connectionString: string, tableName: string, id: string) => await Promise.resolve({ _id: id }),
-        getAll: async (connectionString: string, tableName: string) => await Promise.resolve([{ _id: "a" }, { _id: "b" }]),
-        save: async (connectionString: string, tableName: string, item: object) => await Promise.resolve(item),
-        remove: async (connectionString: string, tableName: string, id: string) => await Promise.resolve(true)
-      }
+        get: async (connectionString: string, tableName: string, id: string) =>
+          await Promise.resolve({ _id: id }),
+        getAll: async (connectionString: string, tableName: string) =>
+          await Promise.resolve([{ _id: "a" }, { _id: "b" }]),
+        save: async (
+          connectionString: string,
+          tableName: string,
+          item: object
+        ) => await Promise.resolve(item),
+        remove: async (
+          connectionString: string,
+          tableName: string,
+          id: string
+        ) => await Promise.resolve(true),
+      },
     };
   });
 
@@ -35,22 +39,25 @@ describe("PgStore", () => {
 
   it("getAll()", async () => {
     assert.equal(
-      await store.getAll().then((items: { _id: string }[]) => items.map(({ _id }) => _id).join("")),
+      await store
+        .getAll()
+        .then((items: { _id: string }[]) =>
+          items.map(({ _id }) => _id).join("")
+        ),
       "ab"
     );
   });
 
   it("save()", async () => {
     assert.equal(
-      await store.save({ name: "test" }).then(({ name }: { name: string; }) => name),
+      await store
+        .save({ name: "test" })
+        .then(({ name }: { name: string }) => name),
       "test"
     );
   });
 
   it("remove()", async () => {
-    assert.equal(
-      await store.remove("test"),
-      true
-    );
+    assert.equal(await store.remove("test"), true);
   });
 });
